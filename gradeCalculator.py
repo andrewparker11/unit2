7e053c1a945b9bbf2612fc87f14b265c6bf808fa
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-13 13:48:54 -0400

    Updated from Brython Server: 9/13/2017 1:48:53 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,3 +1,13 @@
 #Andrew Parker
 #9/13/17
 #gradeCalculator.py - Converts numerical grades to letter grades
+
+grade = float(input('Enter a number: '))
+if 101>grade>92:
+    print('You earned an A')
+if 93>grade>92:
+    print('You earned an A')
+if 101>grade>92:
+    print('You earned an A')
+if 101>grade>92:
+    print('You earned an A')
