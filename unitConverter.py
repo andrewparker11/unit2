9393e0a87d662bff01020ddd2e0f41351b47d7b6
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-14 14:49:04 -0400

    Updated from Brython Server: 9/14/2017 2:49:03 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,14 @@
+#Andrew Parker
+#9/14/17
+#unitConverter.py - converts stuff
 
+print(' 1)Kilometers to Miles ')
+print(' 2)Kilograms to Pounds ')
+print(' 3)Liters to Gallons ')
+print(' 4)Celsius to Fahrenheit ')
+
+uc = int(input('Enter an interger: '))
+if num%2 == 0:
+    print('Your number is even')
+else:
+    print('Your number is odd')
