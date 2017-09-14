53d1b3bda5626b0064331017200bb7e6ce0417fa
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-14 14:33:19 -0400

    Updated from Brython Server: 9/14/2017 2:33:18 PM

--- movie.py
+++ movie.py
@@ -1,3 +1,11 @@
 #Andrew Parker
 #9/13/17
-#movie.py - Gives rating of movie based on age
+#movie.py - Gives most scandalous movie rating you can watch by age
+
+age = int(input('Enter your age: '))
+if age>0:
+    print('The most scandalous movie you can watch is G-rated')
+elif 10>age>7:
+    print('The most scandalous movie you can watch is PG-rated')
+elif 13>age>10:
+    print('The most scandalous movie you can watch is PG-rated')
