0c7cdb5fb8bef0bee6284e4a5ac93bc47b61fbba
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-14 14:34:44 -0400

    Updated from Brython Server: 9/14/2017 2:34:42 PM

--- movie.py
+++ movie.py
@@ -6,6 +6,8 @@ age = int(input('Enter your age: '))
 if age>0:
     print('The most scandalous movie you can watch is G-rated')
 elif 10>age>7:
-    print('The most scandalous movie you can watch is PG-rated')
+    print('The most scandalous movie you can watch is PG rated')
 elif 13>age>10:
-    print('The most scandalous movie you can watch is PG-rated')
+    print('The most scandalous movie you can watch is PG-13 rated with a parent or guardian')
+elif 17>age>13:
+    print('The most scandalous movie you can watch is R rated with a parent or guardian')
