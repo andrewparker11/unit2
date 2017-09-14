1f0f05f2896a8da91c14327f4305689cbd66f6f8
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-14 14:40:28 -0400

    Updated from Brython Server: 9/14/2017 2:40:27 PM

--- oddEven.py
+++ oddEven.py
@@ -3,7 +3,7 @@
 #oddEven.py - Tells whether given integer is odd or even
 
 num = int(input('Enter an interger: '))
-if and num%2 == 0:
+if num%2 == 0:
     print('Your number is even')
 else:
     print('Your number is odd')
