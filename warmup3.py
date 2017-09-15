b1dd3761a89cf04af5ac98df9562a0132d737810
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-15 14:24:36 -0400

    Updated from Brython Server: 9/15/2017 2:24:36 PM

--- warmup3.py
+++ warmup3.py
@@ -2,7 +2,7 @@
 #9/15/17
 #warmup3
 
-num = int(input('Please give a number' )
+num = int(input('Please input a number' )
 
 if num%3 and num%2:
     print('Your number is both divisible by 2 and 3' )
