9126897476b82e2e5565479458d49ebace367230
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-15 14:31:54 -0400

    Updated from Brython Server: 9/15/2017 2:31:54 PM

--- warmup3.py
+++ warmup3.py
@@ -4,13 +4,12 @@
 
 num = float(input('Please input a number '))
 
-
-if num%3 == 0:
+if num%3 == 0 and num%2 == 0:
+    print('Your number is both divisible by 2 and 3' )
+elif num%3 == 0:
      print('Your number is divisible by 3' )
 elif num%2 == 0:
      print('Your number is divisible by 2' )
-elif num%3 == 0 and num%2 == 0:
-    print('Your number is both divisible by 2 and 3' )
 else:
     print('neither' )
     
