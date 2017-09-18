02abf7595a2b252fca18f2c8ae65806cb4518b74
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-18 14:48:56 -0400

    Updated from Brython Server: 9/18/2017 2:48:55 PM

--- insulter.py
+++ insulter.py
@@ -7,5 +7,14 @@ from random import randint
 num = randint(1,5)
 name = (input('Enter your name'))
 
-if num = 1:
-    print('Fak off m8')
+if num is 1:
+    print('Your the type of person who would try to save a fish from drowning')
+elif num is 2:
+    print('You would try to turn on a TV with calculator ')
+elif num is 3:
+    print(' ')
+elif num is 4:
+    print(' ')
+elif num is 5:
+    print(' ')
+    
