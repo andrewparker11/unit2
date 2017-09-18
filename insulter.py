fb0f677928b4730293a24e814cac7ad363f88608
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-18 14:46:04 -0400

    Updated from Brython Server: 9/18/2017 2:46:03 PM

--- insulter.py
+++ insulter.py
@@ -2,6 +2,10 @@
 #9/18/17
 #insulter.py - I dont need to explain what this program does you IDIOT!
 
+from random import randint 
 
+num = randint(1,5)
+name = (input('Enter your name'))
 
-
+if num = 1:
+    print('Fak off m8')
