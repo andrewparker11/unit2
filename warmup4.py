50f313d407868e03ca3ec27d827bc0c7b001b9c4
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-18 14:21:25 -0400

    Updated from Brython Server: 9/18/2017 2:21:24 PM

--- warmup4.py
+++ warmup4.py
@@ -3,8 +3,9 @@
 #warmup4.py - if has 7 or divisble by 7 says Buzz
 
 num = int(input('Input a Number'))
+num = string(input('Input the same number'))
 
-if num is 7:
+if num has '7':
     print('Buzz')
 else:
     print(num)
