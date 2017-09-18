f156091213a71518d9b8df2d552208b1825a1a25
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-18 14:38:18 -0400

    Updated from Brython Server: 9/18/2017 2:38:18 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -6,18 +6,18 @@ color = string(input('Pick red or blue '))
 num = string(input('Pick a number from 1-4 '))
 
 if color=='blue' and num==1:
-    print('')
+    print('You will meet a bunny. A killer bunny')
 elif color=='blue' and num==2:
-    print(')
+    print('You will be trampled in  coop stampede')
 elif color=='blue' and num==3:
-    print(')
+    print('You win 15 dollars from a beauty contest')
 elif color=='blue' and num==4:
-    print(')
+    print('You will contract lyme disease. Twice')
 elif color=='red' and num==1:
-    print(')
+    print('You will go skydiving and your parachute will open. On impact..')
 elif color=='red' and num==2:
-    print(')
+    print('You will narrowly avoid a car crash. And fall into a manhole.')
 elif color=='red' and num==3:
-    print(')
+    print('Youll gain 1000000 dollars from operations that we both know arent legal.')
 elif color=='red' and num==4:
-    print(')
+    print('You win an award. The Darwin award.')
