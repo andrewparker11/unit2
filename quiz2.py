f7c0117d178990cb5fd660bf21708913e536f5c4
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-25 14:26:36 -0400

    Updated from Brython Server: 9/25/2017 2:26:37 PM

--- quiz2.py
+++ quiz2.py
@@ -2,8 +2,8 @@
 #9/25/17
 #quiz2.py - Analyzes two numbers and quizes you on them
 
-num1 = int(input('Enter a number: ')
-num2 = int(input('Enter a second number: ')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a second number: '))
 if num1>num2:
     print('The first number is bigger' )
 elif num2>num1:
